6c9a7ab22f334fe1edaa10f968a0075a512a9a79
Oleh Meserenko <meserenko.oleg@gmail.com>
2023-01-13 19:42:10 -0500

    Add RestaurantDetailsScreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet, View } from 'react-native'
-import { HomeScreen } from './src/screens/HomeScreen'
+import { RestaurantDetailsScreen } from './src/screens/RestaurantDetailsScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <RestaurantDetailsScreen />
     </View>
   )
 }
